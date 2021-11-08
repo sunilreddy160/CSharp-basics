fb2616c14620ab425bdd6dd342ed58675588e066
sunilreddy160 <sunilrampatruni@gmail.com>
2021-11-08 21:50:28 +0530

    my commit

---
+++ CSharp input user/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CSharp_input_user
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           /* Console.WriteLine("Enter the name:");
+            string a = Console.ReadLine();
+            Console.WriteLine(a);
+            Console.ReadLine();*/
+            //sum of three integer values uof user inputs
+           /* Console.WriteLine("enter first num:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter first num:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter first num:");
+            int c = Convert.ToInt32(Console.ReadLine());
+            int sum = a + b + c;
+            Console.WriteLine("sum of three numbers is: " + sum);
+           */
+            //ex2
+            Console.WriteLine("enter first name:");
+            string a = Console.ReadLine();
+            Console.WriteLine("user name is " + " "+ a);
+
+        }
+    }
+}
